f50b592bb5a695240c0342a2ffad1d545352ba48
lazineer <lazineer7@gmail.com>
2014-04-30 21:47:49 +0900

    검색기능

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,46 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("LoadAllItems", function(request, response) {
+	var query = new Parse.Query("BBS");
+
+	query.descending("createdAt");
+
+	query.find({
+		success: function(results) {
+			response.success(results);
+		},
+
+		error: function(error) {
+			// error is an instance of Parse.Error.
+			response.error("글목록 로딩 실패");
+		}
+	});
+});
+
+Parse.Cloud.define("LoadSpecificItems", function(request, response) {
+	var query = new Parse.Query("BBS");
+
+	if(request.params.parentOptVal === 'Title') {
+		query.contains('Title', request.params.searchWord);
+	}
+	else {
+		query.contains(request.params.parentOptVal, request.params.childOptVal);
+	}
+
+	query.descending("createdAt");
+	
+	request.params.childOptVal
+
+	query.find({
+		success: function(results) {
+			response.success(results);
+		},
+
+		error: function(error) {
+			// error is an instance of Parse.Error.
+			response.error("글목록 로딩 실패");
+		}
+	});
+});
--- public/common.js
+++ public/common.js
@@ -1,3 +1,126 @@
+var num_start;
+var num_end;
+var num_total;
+
+function getURLParameter(name) {
+		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
+
+function LoadAllItems(isDefault){
+	
+	//클라우드 코드 내에 선언을 하면 안 되네 ㅡㅡ;;
+	var obj = {};
+	var item;
+
+	Parse.Cloud.run('LoadAllItems', {}, {			
+		
+		success: function(items) {
+
+			num_total = items.length;
+
+			
+			if(isDefault)
+				num_start = 0;
+			else
+				num_start = num_end;
+
+			num_end = num_start + 10;
+			
+			for (var i = num_start; i < num_end; i++) { 
+				item = items[i];
+				obj.strId = item.id;
+				obj.strBranch = item.get("Branch");
+				obj.strPU = item.get("PU");
+				obj.strTitle = item.get("Title");
+				obj.strContent = item.get("Content");//사용 안 함
+				obj.bPublic = item.get("Public");
+				obj.date = item.createdAt;
+
+				$('#div_list').append("<a href='read.html?id=" + obj.strId + "'>" + "<div style='width: 100%; border-bottom: 2px solid black; padding-top : 15px; padding-bottom: 15px'>"+
+				"<div style='float: left; margin-left: 10px; background-color: #808080; width: 60px; height: 20px; color: white; font-weight: bolder; text-align: center; padding-top: 5px; border-radius: 5px; font-size: 13px'>"+obj.strBranch+"</div>"
+									+"<div style='float: left; margin-left: 30px; height: 30px; font-weight: bolder;'>" + obj.strTitle + "</div>"
+									+"<br><br><div style='float: left;  margin-left: 10px; margin-top: -10px; width: 60px; height: 30px; color: black; font-weight: bolder; text-align: center; font-size: 13px'>" + obj.strPU + "</div>"
+									+"<div style='float: right; margin-top: -10px; height: 30px; font-weight: bolder; font-size: 13px'>" +obj.date.getFullYear() + '-' + (obj.date.getMonth() + 1) + '-' + obj.date.getDate() + "&nbsp&nbsp" + "</div>"
+				+"</div>" + "</a>");
+			}
+
+			if(isDefault) {
+				$('#div_list').append("<div class='next'><a href='page.html'>next</a></div>");
+				$('#div_list').jscroll();
+			}
+			else {
+				$("#div_list2").parent().append("<div class='next'><a href='page.html'>next</a></div>");
+				$("#div_list2").parent().jscroll();
+				$("#div_list2").remove();
+			}
+		
+		},
+		error: function(error) {
+			alert(error);
+		}
+	});	
+}
+
+function LoadSpecificItems(isDefault, parentOptVal, childOptVal, searchWord){
+	
+	//클라우드 코드 내에 선언을 하면 안 되네 ㅡㅡ;;
+	var obj = {};
+	var item;
+
+	Parse.Cloud.run('LoadSpecificItems', {parentOptVal:parentOptVal, childOptVal:childOptVal, searchWord:searchWord}, {			
+		
+		success: function(items) {
+
+			num_total = items.length;
+
+			
+			if(isDefault)
+				num_start = 0;
+			else
+				num_start = num_end;
+
+			num_end = num_start + 10;
+			
+			for (var i = num_start; i < num_end; i++) { 
+				item = items[i];
+				obj.strId = item.id;
+				obj.strBranch = item.get("Branch");
+				obj.strPU = item.get("PU");
+				obj.strTitle = item.get("Title");
+				obj.strContent = item.get("Content");//사용 안 함
+				obj.bPublic = item.get("Public");
+				obj.date = item.createdAt;
+
+				$('#div_list').append("<a href='read.html?id=" + obj.strId + "'>" + "<div style='width: 100%; border-bottom: 2px solid black; padding-top : 15px; padding-bottom: 15px'>"+
+				"<div style='float: left; margin-left: 10px; background-color: #808080; width: 60px; height: 20px; color: white; font-weight: bolder; text-align: center; padding-top: 5px; border-radius: 5px; font-size: 13px'>"+obj.strBranch+"</div>"
+									+"<div style='float: left; margin-left: 30px; height: 30px; font-weight: bolder;'>" + obj.strTitle + "</div>"
+									+"<br><br><div style='float: left;  margin-left: 10px; margin-top: -10px; width: 60px; height: 30px; color: black; font-weight: bolder; text-align: center; font-size: 13px'>" + obj.strPU + "</div>"
+									+"<div style='float: right; margin-top: -10px; height: 30px; font-weight: bolder; font-size: 13px'>" +obj.date.getFullYear() + '-' + (obj.date.getMonth() + 1) + '-' + obj.date.getDate() + "&nbsp&nbsp" + "</div>"
+				+"</div>" + "</a>");
+			}
+
+			if(isDefault) {
+				//$('#div_list').append("<div class='next'><a href='page.html'>next</a></div>");
+				//$('#div_list').jscroll();
+			}
+			else {
+				$("#div_list2").parent().append("<div class='next'><a href='page.html'>next</a></div>");
+				$("#div_list2").parent().jscroll();
+				$("#div_list2").remove();
+			}
+		
+		},
+		error: function(error) {
+			alert(error);
+		}
+	});	
+}
+
+function nextPage(){
+	location = "page.html?s="+num_start+"&e="+num_end;
+	//http://192.168.137.1:8020/vom_ing/public/index.html
+}
+
 function AddOption(optType, optID){
 		//document.getElementById(optID).innerHTML = "";
 		
